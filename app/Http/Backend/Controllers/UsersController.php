d82c980f440b8262aec52aaed7bf692b23db5fb4
thienvuong <huydq.it@gmail.com>
2016-04-06 09:41:41 +0700

    delete create and update user in admin

--- app/Http/Backend/Controllers/UsersController.php
+++ app/Http/Backend/Controllers/UsersController.php
@@ -4,8 +4,6 @@ namespace App\Http\Backend\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Backend\Requests;
-use App\Http\Backend\Requests\CreateUsersRequest;
-use App\Http\Backend\Requests\UpdateUsersRequest;
 use App\User;
 use Flash;
 use Hash;
@@ -24,31 +22,6 @@ class UsersController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('backend.users.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request request create user
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function store(CreateUsersRequest $request)
-    {
-        $data = $request->all();
-        User::create($data);
-        Flash::success('Create new admin user success.');
-        return redirect()->route('admin.users.index');
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param int $id id admin user
@@ -59,36 +32,6 @@ class UsersController extends Controller
     {
         $user = User::findOrFail($id);
         return view('backend.users.view', compact('user'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id id user
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $user = User::findOrFail($id);
-        return view('backend.users.edit', compact('user'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request request update user
-     * @param int                      $id      id users
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateUsersRequest $request, $id)
-    {
-        $user = User::findOrFail($id);
-        $data = $request->all();
-        $user->update($data);
-        Flash::success('Update users success.');
-        return redirect()->route('admin.users.index');
     }
 
     /**
